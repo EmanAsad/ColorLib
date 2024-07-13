50fbc84a350a1fd119fb2df45ace2f5c060cae7a
Eman Asad <106083858+EmanAsad@users.noreply.github.com>
2024-07-13 08:42:52 -0700

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -46,33 +46,29 @@ const Main = () => {
                         {slides.map((slide, index) => (
                             <SwiperSlide key={index} className="relative">
                                 <div
-                                    className={classNames('min-h-[700px] md:min-h-[550px] sm:min-h-[500px] xs:min-h-[410px] bg-cover bg-center', slide.img)}
+                                    className={classNames('min-h-[700px] md:min-h-[550px] sm:min-h-[500px] xs:min-h-[410px] bg-cover bg-center')}
                                     style={{ backgroundImage: `url(${slide.img})` }}
                                 >
-                                    <div className={classNames("absolute inset-0 flex flex-col justify-center p-20", {
-                                        'items-start text-left ml-20': index === 0 && slide.alignment !== 'right',
-                                        'items-start text-left ml-[600px]': index === 1 && slide.alignment === 'right',
+                                    <div className={classNames("absolute inset-0 flex flex-col justify-center p-6 md:p-20", {
+                                        'items-start text-left ml-6 md:ml-20': index === 0 && slide.alignment !== 'right',
+                                        'items-end text-right mr-6 md:mr-20': index === 1 && slide.alignment === 'right',
                                     })}>
                                         <div className="hero-caption">
-                                            <span className={classNames('text-5xl m-7 text-orange-500 animate__animated rochester-regular', {
+                                            <span className={classNames('text-4xl md:text-5xl lg:text-6xl m-2 md:m-7 text-orange-500 animate__animated rochester-regular', {
                                                 'animate__bounceIn': index === 0,
-                                                'animate__fadeInUp pl-20': index === 1,
+                                                'animate__fadeInUp': index === 1,
                                             })}>{slide.heading}</span>
-                                            <h1 className={classNames('text-xxl font-bold mb-2 animate__animated', {
-                                                'text-2xl md:text-3xl': index === 0,
+                                            <h1 className={classNames('text-2xl md:text-3xl lg:text-4xl font-bold mb-2 animate__animated', {
                                                 'animate__bounceIn': index === 0,
-                                                'animate__fadeInUp pl-10 text-3xl ': index === 1,
+                                                'animate__fadeInUp': index === 1,
                                             })}>{slide.heading2}</h1>
-                                            <p className={classNames('text-1  mt-6 mb-10 text-gray animate__animated text-center', {
-                                                'animate__fadeInUp ': index === 0,
-                                                'animate__fadeInUp ': index === 1,
-                                                'animate__delay-2s  mt-6 mb-10': index === 0,
-                                                'animate__delay-2s  mt-6 mb-10 text-2': index === 1,
-                                                'text-sm': index === 0
+                                            <p className={classNames('text-sm md:text-base lg:text-lg mt-4 md:mt-6 mb-4 md:mb-10 text-gray-700 animate__animated', {
+                                                'animate__fadeInUp': index === 0 || index === 1,
                                             })} dangerouslySetInnerHTML={{ __html: slide.text }}></p>
-                                            <a href={slide.link} className={classNames('hero-btn ml-10 bg-black hover:bg-gray-600 text-white font-bold py-3 px-8 rounded animate__animated', {
-                                                'animate__fadeInUp': index === 0,
-                                                'animate__delay-2s': index === 0,
+                                            <a href={slide.link} className={classNames('hero-btn bg-black hover:bg-gray-600 text-white font-bold py-2 md:py-3 px-4 md:px-8 rounded animate__animated', {
+                                                'animate__fadeInUp': index === 0 || index === 1,
+                                                'ml-0 md:ml-10': index === 0,
+                                                'mr-0 md:mr-10': index === 1,
                                             })}>
                                                 {slide.button}
                                             </a>
@@ -82,14 +78,14 @@ const Main = () => {
                             </SwiperSlide>
                         ))}
                     </Swiper>
-                    <div className="absolute top-1/2 transform -translate-y-1/2 left-4">
+                    <div className="absolute top-1/2 transform -translate-y-1/2 left-7">
                         <button className="swiper-button-prev bg-black bg-opacity-50 p-2 rounded w-10 h-10 flex items-center justify-center">
                             <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
                                 <path fillRule="evenodd" d="M9.293 5.293a1 1 0 011.414 0l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414-1.414L12.586 11H3a1 1 0 110-2h9.586L9.293 6.707a1 1 0 010-1.414z" clipRule="evenodd" />
                             </svg>
                         </button>
                     </div>
-                    <div className="absolute top-1/2 transform -translate-y-1/2 right-5">
+                    <div className="absolute top-1/2 transform -translate-y-1/2 right-10">
                         <button className="swiper-button-next bg-black bg-opacity-50 p-2 rounded w-10 h-10 flex items-center justify-center">
                             <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
                                 <path fillRule="evenodd" d="M10.707 6.707a1 1 0 010 1.414L7.414 11l3.293 3.293a1 1 0 11-1.414 1.414l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 0z" clipRule="evenodd" />
